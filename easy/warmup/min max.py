ff1899d897618a10eb55fa1733af2f7ed832d78a
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-15 01:13:03 +0530

    Update min max.py

--- easy/warmup/min max.py
+++ easy/warmup/min max.py
@@ -7,16 +7,6 @@ import re
 import sys
 
 
-def selection_sort(a):
-    sample = a.copy()
-    sorted_list = []
-    
-    while sample:
-        min_val = min(sample)
-        sorted_list.append(min_val)
-        sample.remove(min_val)
-
-    return sorted_list
 
 
 #
@@ -26,14 +16,15 @@ def selection_sort(a):
 #
 
 def miniMaxSum(arr):
-    # Write your code here
-    max_val=sum(arr[1:])
-    min_val=sum(arr[:-1])
-    print(min_val,max_val )
+    
+    max_val=sum(arr)-min(arr)
+    min_val=sum(arr)-max(arr)
+    
+    print(min_val, max_val)
 
 if __name__ == '__main__':
 
     arr = list(map(int, input().rstrip().split()))
-    arr=selection_sort(arr)
+    
 
     miniMaxSum(arr)
