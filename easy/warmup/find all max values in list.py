fe5bf1057aa8b023f26b0f71c9f76b8ef8a710ab
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-25 23:24:52 +0530

    Create find all max values in list.py
    You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
    
    Example
    
    
    The maximum height candles are  units high. There are  of them, so return .
    
    Function Description
    
    Complete the function birthdayCakeCandles in the editor below.
    
    birthdayCakeCandles has the following parameter(s):
    
    int candles[n]: the candle heights
    Returns
    
    int: the number of candles that are tallest

---
+++ easy/warmup/find all max values in list.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    max_count = 0
+    max_val=max(candles)
+    for i in candles:
+        if i == max_val:
+            max_count+=1
+    return max_count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
