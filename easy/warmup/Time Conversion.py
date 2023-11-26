fdced445969edd05b872771d47444d07c6a5976b
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-27 01:35:49 +0530

    Update Time Conversion.py

--- easy/warmup/Time Conversion.py
+++ easy/warmup/Time Conversion.py
@@ -15,10 +15,16 @@ import sys
 
 def timeConversion(s):
     str_list = list(map(str, s.split(':')))
+    last=str_list[-1]
+    
+    if str_list[0] == '12' :
+        str_list[0]='00'
     if 'PM' in str_list[-1]:
-        str_list[0]=str(int(str[0])+12)
-        str_list[-1]=str
-    else:
+        str_list[0]=str(int(str_list[0])+12)
+
+    str_list[-1]=last[:2]
+
+    return ':'.join(str_list)
         
 
 if __name__ == '__main__':
